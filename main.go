747aaca0bac64c277140cab185c40f97da3330c3
Constantine <tmail99@gmail.com>
2025-09-08 17:29:48 +0300

    создаем и заполняем таблицу

--- main.go
+++ main.go
@@ -19,9 +19,13 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/csv"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -130,7 +134,135 @@ func HealthCheckHandler() http.HandlerFunc {
 	}
 }
 
+// ********************************************************************//
+
+type Video struct {
+	Id          string    // video_id
+	Title       string    // title
+	PublishTime time.Time // publish_time
+	Tags        []string  // tags
+	Views       int       // views
+}
+
+func readVideoCSV(csvFile string) ([]Video, error) {
+	// открываем csv файл
+	file, err := os.Open(csvFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	var videos []Video
+
+	// определим индексы нужных полей
+	const (
+		Id          = 0 // video_id
+		Title       = 2 // title
+		PublishTime = 5 // publish_time
+		Tags        = 6 // tags
+		Views       = 7 // views
+	)
+
+	// конструируем Reader из пакета encoding/csv
+	// он умеет читать строки csv-файла
+	r := csv.NewReader(file)
+	// пропустим первую строку с именами полей
+	if _, err := r.Read(); err != nil {
+		return nil, err
+	}
+
+	for {
+		// csv.Reader за одну операцию Read() считывает одну csv-запись
+		l, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		// инициализируем целевую структуру,
+		// в которую будем делать разбор csv-записи
+		v := Video{
+			Id:    l[Id],
+			Title: l[Title],
+		}
+		// парсинг строковых записей в типизированные поля структуры
+		if v.PublishTime, err = time.Parse(time.RFC3339, l[PublishTime]); err != nil {
+			return nil, err
+		}
+		tags := strings.Split(l[Tags], "|")
+		for i, v := range tags {
+			tags[i] = strings.Trim(v, `"`)
+		}
+		v.Tags = tags
+		if v.Views, err = strconv.Atoi(l[Views]); err != nil {
+			return nil, err
+		}
+		// добавляем полученную структуру в слайс
+		videos = append(videos, v)
+	}
+	return videos, nil
+}
+
+func insertVideos(ctx context.Context, db *sql.DB, videos []Video) error {
+	for _, v := range videos {
+		// В postgresql  плейсхолдеры (или «заполнители») это $1,$2, ...
+		// В sqlite это ?,?, ...
+		_, err := db.ExecContext(ctx,
+			"INSERT INTO videos (video_id, title, publish_time, tags, views)"+
+				" VALUES($1,$2,$3,$4,$5)", v.Id, v.Title, v.PublishTime,
+			strings.Join(v.Tags, `|`), v.Views)
+		// // Добавить в конец запроса- проверка на дубликаты
+		// "ON CONFLICT (video_id) DO UPDATE SET"+
+		// "title = EXCLUDED.title,"+
+		// "views = EXCLUDED.views"
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func DbFunc() {
+	// открываем соединение с БД
+	// DataSourceName
+	dsn := "postgres://postgres:qwerty@localhost:5432/postgres?sslmode=disable"
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	//..
+	ctx := context.Background()
+	_, err = db.ExecContext(ctx, `CREATE TABLE IF NOT EXISTS videos (
+	    "video_id" TEXT,
+	    "title" TEXT,
+	    "publish_time" TEXT,
+	    "tags" TEXT,
+	    "views" INTEGER
+	  )`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// читаем записи из файла в слайс []Video вспомогательной функцией
+	videos, err := readVideoCSV(".\\USvideos.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// записываем []Video в базу данных
+	err = insertVideos(ctx, db, videos)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Всего csv-записей %v\n", len(videos))
+}
+
 func main() {
+	// Подключение к db и работа с ней
+	DbFunc()
 	// 1. роутер, то, что умеет работать с путями
 	router := chi.NewRouter()
 
