f5708e5a6184b0b830cf36e88cb668b1c40259f7
Constantine <tmail99@gmail.com>
2025-10-05 18:32:14 +0300

    http.HandlerFunc , http.Handler и http.NewServeMux

--- main.go
+++ main.go
@@ -1,145 +1,39 @@
 package main
 
 import (
-	"context"
-	//"database/sql"
-	"database/sql/driver"
-	"errors"
 	"fmt"
-	"log"
-	"strings"
-
-	"github.com/jmoiron/sqlx"
-
-	_ "github.com/lib/pq"
+	"net/http"
+	"time"
 )
 
-type Tags []string
+// helloweb - Snippet for sample hello world webapp (Go)
 
-// В этом примере преобразуем данные из таблицы postgresql
-// в пользовательский тип - структуру Video
-type Video struct {
-	Id    string
-	Title string
-	Tags  Tags
-	Views int
+// Это и есть "простая" функция в качестве обработчика
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World! %s", time.Now())
 }
 
-// приводит сложные типы и структуры к простому типу
-type Valuer interface {
-	Value() (driver.Value, error)
-}
-
-// приводит простой тип к сложным типам и структурам Go
-type Scanner interface {
-	Scan(src any) error
-}
-
-// Value — функция реализующая интерфейс driver.Valuer
-func (tags Tags) Value() (driver.Value, error) {
-	// преобразуем []string в string
-	if len(tags) == 0 {
-		return "", nil
-	}
-	return strings.Join(tags, "|"), nil
-}
-
-func (tags *Tags) Scan(value interface{}) error {
-	// если `value` равен `nil`, будет возвращён пустой массив
-	if value == nil {
-		*tags = Tags{}
-		return nil
-	}
-
-	sv, err := driver.String.ConvertValue(value)
-	if err != nil {
-		return fmt.Errorf("cannot scan value. %w", err)
-	}
-
-	v, ok := sv.(string)
-	if !ok {
-		return errors.New("cannot scan value. cannot convert value to string")
-	}
-	*tags = strings.Split(v, "|")
-
-	// удаляем кавычки у тегов
-	for i, v := range *tags {
-		(*tags)[i] = strings.Trim(v, `"`)
-	}
-	return nil
-}
-
-// получим идентификатор, наименование и теги у роликов
-func QueryTagVideos(ctx context.Context, db *sqlx.DB, limit int) ([]Video, error) {
-	videos := make([]Video, 0, limit)
-	// // самый простой
-	// query := `
-	// 	SELECT video_id, tags ,views
-	// 	FROM videos
-	// 	ORDER BY views
-	// 	LIMIT $1
-	// `
-	query := `
-		SELECT video_id, MAX(tags) as tags, MAX(views) as views
-		FROM videos
-		GROUP BY video_id
-		ORDER BY MAX(views) DESC
-		LIMIT $1
-		`
-
-	rows, err := db.QueryContext(ctx, query, limit)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var v Video
-		// все теги должны автоматически преобразоваться в слайс v.Tags
-		err = rows.Scan(&v.Id, &v.Tags, &v.Views)
-		if err != nil {
-			return nil, err
-		}
-		videos = append(videos, v)
-	}
-
-	err = rows.Err()
-	if err != nil {
-		return nil, err
-	}
-	return videos, nil
+func twet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "TTT")
 }
 
 func main() {
-	// db, err := sql.Open("sqlite", "video.db")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer db.Close()
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /", greet)
 
-	// открываем соединение с БД
-	// DataSourceName
-	dsn := "postgres://postgres:qwerty@localhost:5432/postgres?sslmode=disable"
-	db, err := sqlx.Open("postgres", dsn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	//..
+	// // http.HandlerFunc— это удобный адаптер,
+	// // который позволяет простой функции выполнять "контракт"
+	// // http.Handler на обработку HTTP-запросов (чтобы это не значило),
+	// // упрощая использование простых функций в качестве обработчиков.
+	// http.HandleFunc("/", greet)
 
-	// получим идентификатор, наименование и теги у роликов
-	limit := 5
-	list, err := QueryTagVideos(context.Background(), db, limit)
-	if err != nil {
-		panic(err)
-	}
-	// для теста проверим, какие строки содержит v.Tags
-	// выведем по 4 первых тега
-	for _, v := range list {
-		length := 4
-		if len(v.Tags) < length {
-			length = len(v.Tags)
-		}
-		fmt.Println(strings.Join(v.Tags[:length], " # "))
-	}
+	// Это не правильно, оставлю для понимания
+	http.HandleFunc("/two", twet)
+
+	// // The handler is typically nil, in which case [DefaultServeMux] is used.
+	// // Обработчик (второй параметр) по умолчанию равен nil, в этом случае используется [DefaultServeMux].
+	// // Его использование рекомендуется, только в простых, тестовых приложениях.
+	// // В рабочих приложениях следует использовать http.NewServeMux или сторонние роутеры
+	// http.ListenAndServe("localhost:8080", nil)
+	http.ListenAndServe("localhost:8080", mux)
 }
